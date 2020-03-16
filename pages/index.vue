9afaa1c913d1ac78acc75178863fe72eb88235da
[vikatgen] <[vikat.gen@gmail.com]>
2020-03-16 15:40:48 +0200

    Project ready for module one development

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,7 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-vue-on-steroid
-      </h1>
-      <h2 class="subtitle">
-        My first-rate Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <section class="container">
+    <h1 class="title">My Nuxt Blog App</h1>
+  </section>
 </template>
 
 <script>
