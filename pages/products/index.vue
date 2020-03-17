29cdb42fcaf6d80cf5bc0b7d6278d97e4da55e44
[vikatgen] <[vikat.gen@gmail.com]>
2020-03-17 13:36:44 +0200

    Module 1 ending wrapup

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -3,7 +3,7 @@
 		<h1>The Products Page</h1>
 		<div>
 			<router-link to="/products/1">Products with ID 1</router-link>
-			<router-link to="/products/2">Products with ID 2</router-link>
+			<router-link to="/products/2">Products with ID 2</router-link>					
 		</div>
 	</div>
 </template>
